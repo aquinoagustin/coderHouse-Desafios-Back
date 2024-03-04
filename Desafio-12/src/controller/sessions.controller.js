7b9ba4e422d70babdb09dfad4a5a637ba06cd8b4
Agus2020 <aagustin067@gmail.com>
2024-03-04 20:19:25 -0300

    Agregado control de errores

--- Desafio-12/src/controller/sessions.controller.js
+++ Desafio-12/src/controller/sessions.controller.js
@@ -1,45 +1,79 @@
 class SessionController{
-    static register = async(req,res) => {
-        res.send({status:'success',message:'User registrado'})
-      }
-
-    static failRegister = async(req,res)=>{
-        console.log('Fallo el registro');
-        res.send({error:'Fallo en el registro'})
-    }      
-    static failLogin = (req,res)=>{
-        res.send({error:'fail login'})
+  static register = async(req,res) => {
+    try {
+      res.send({status:'success',message:'User registrado'})
+    } catch (error) {
+      res.status(500).send({
+        error:error.message
+      })
     }
-    static github = async (req,res)=>{};
 
-    static githubCallBack = async (req,res)=>{
-        req.session.user = req.user;
-        res.redirect('/');
-    }    
-    static login =   async(req,res) => {
-        if(!req.user){
-          return res.status(400).send({status:'error'})
-        }
-        req.session.user = {
-          first_name:req.user.first_name,
-          last_name:req.user.last_name,
-          age:req.user.age,
-          email:req.user.email,
-        }
-        res.send({status:'success',payload:req.user})
+    }
+
+  static failRegister = async(req,res)=>{
+      try {
+        res.send({error:'Fallo en el registro'})
+      } catch (error) {
+        res.status(500).send({
+          error:error.message
+        })
       }
-    
-    static logout = (req, res) => {
-        req.session.destroy((err) => {
-          if (err) {
-            return res.status(500).send({
-              status: "error",
-              error: "No se pudo desloguear",
-            });
-          }
-          res.redirect("/");
-        });
+
+  }      
+  static failLogin = (req,res)=>{
+    try {
+      res.send({error:'fail login'})
+    } catch (error) {
+      res.status(500).send({error:error.message})
+    }
+
+  }
+  static github = async (req,res)=>{};
+
+  static githubCallBack = async (req,res)=>{
+    try {
+      req.session.user = req.user;
+      res.redirect('/');
+    } catch (error) {
+      res.status(500).send({error:error.message})
+    }
+
+  }    
+  static login =   async(req,res) => {
+    try {
+      if(!req.user){
+        return res.status(400).send({status:'error'})
       }
+      req.session.user = {
+        first_name:req.user.first_name,
+        last_name:req.user.last_name,
+        age:req.user.age,
+        email:req.user.email,
+      }
+      res.send({status:'success',payload:req.user})  
+    } catch (error) {
+      res.status(500).send({error:error.message})
+    }
+
+    }
+  
+  static logout = (req, res) => {
+    try {
+      req.session.destroy((err) => {
+        if (err) {
+          return res.status(500).send({
+            status: "error",
+            error: "No se pudo desloguear",
+          });
+        }
+        res.redirect("/");
+      });
+    } catch (error) {
+      res.status(500).send({error:error.message})
+    }
+
+
+    }
 
 }
 
